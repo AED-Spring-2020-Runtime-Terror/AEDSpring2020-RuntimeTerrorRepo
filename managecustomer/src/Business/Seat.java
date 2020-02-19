52e04e292d5bc393904ce23542ecad0785e1feb7
Rahul Mahadev <bobba.r@husky.neu.edu>
2020-02-19 16:39:31 -0500

    ManageCustomer

---
+++ managecustomer/src/Business/Seat.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author bobba
+ */
+public class Seat {
+    
+    private boolean isAvailable = true;
+    private String seatNumber;
+    private SeatType seatType;
+    
+     public boolean isIsAvailable() {
+        return isAvailable;
+    }
+
+    public void setIsAvailable(boolean isAvailable) {
+        this.isAvailable = isAvailable;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public SeatType getSeatType() {
+        return seatType;
+    }
+
+    public void setSeatType(SeatType seatType) {
+        this.seatType = seatType;
+    }
+    
+     @Override
+    public String toString() {
+        return "Seat{" + "isAvailable=" + isAvailable + ", seatNumber=" + seatNumber + ", seatType=" + seatType + '}';
+    }
+    
+    
+}
