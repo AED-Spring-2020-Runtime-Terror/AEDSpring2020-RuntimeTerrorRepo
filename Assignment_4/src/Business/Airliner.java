197baa5b2275a14797864910591a229ecfcba9f6
srushti104 <dhamangaonkar.s@husky.edu.neu>
2020-02-20 15:58:14 -0500

    interfaces structure

---
+++ Assignment_4/src/Business/Airliner.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author srush
+ */
+public class Airliner {
+    private Fleet fleet;
+    private String airlineName;
+    private FlightSchedule flightSchedule;
+    
+    public Fleet getFleet() {
+        return fleet;
+    }
+
+    public void setFleet(Fleet fleet) {
+        this.fleet = fleet;
+    }
+
+    public String getAirlineName() {
+        return airlineName;
+    }
+
+    public void setAirlineName(String airlineName) {
+        this.airlineName = airlineName;
+    }
+
+    public FlightSchedule getFlightSchedule() {
+        return flightSchedule;
+    }
+
+    public void setFlightSchedule(FlightSchedule flightSchedule) {
+        this.flightSchedule = flightSchedule;
+    }
+
+    @Override
+    public String toString() {
+        return this.airlineName;
+    }
+     
+}
---
+++ Assignment_4/src/Business/Fleet.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+/**
+ *
+ * @author srush
+ */
+public class Fleet {
+    private String id;
+    private String location;
+    private int planesOnFleet;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getPlanesOnFleet() {
+        return planesOnFleet;
+    }
+
+    public void setPlanesOnFleet(int planesOnFleet) {
+        this.planesOnFleet = planesOnFleet;
+    }
+
+    @Override
+    public String toString() {
+        return "Fleet{" + "id=" + id + ", location=" + location + ", planesOnFleet=" + planesOnFleet + '}';
+    }
+    
+}
