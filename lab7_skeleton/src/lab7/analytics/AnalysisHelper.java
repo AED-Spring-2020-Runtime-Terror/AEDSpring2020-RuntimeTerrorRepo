866c342f49bad31ac08169212ebaf29c74381bed
Rahul Mahadev <bobba.r@husky.neu.edu>
2020-02-22 15:42:08 -0500

    Top5InactiveUsersWithComments

--- lab7_skeleton/src/lab7/analytics/AnalysisHelper.java
+++ lab7_skeleton/src/lab7/analytics/AnalysisHelper.java
@@ -4,11 +4,15 @@
  * and open the template in the editor.
  */
 package lab7.analytics;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.List;
 import lab7.entities.Comment;
 import lab7.entities.Post;
-
+import lab7.entities.User;
 /**
  *
  * @author harshalneelkamal
@@ -37,4 +41,25 @@ public class AnalysisHelper {
         System.out.println("Post with most likdes " + max + "\n"
                 + posts.get(maxId));
     }
+     public void inactiveUsers_based_on_comments() {
+
+        Map<Integer, User> users = DataStore.getInstance().getUsers();
+
+        List<User> userCommentList = new ArrayList<>(users.values());
+
+        Collections.sort(userCommentList, new Comparator<User>() {
+            @Override
+            public int compare(User o1, User o2) {
+                return o1.getComments().size() - o2.getComments().size();
+            }
+        });
+        System.out.println("% top 5 inactive users based on comments:");
+        for (int i = 0; i < userCommentList.size() && i < 5; i++) {
+            System.out.println(userCommentList.get(i));
+        }
+
+    }
 }
+
+
+
