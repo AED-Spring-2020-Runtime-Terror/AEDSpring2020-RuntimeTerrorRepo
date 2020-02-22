1112747b618e380d3ebcc17f9085b390ad7201c5
bharadwaj-ni <bharadwaj.ni@husky.neu.edu>
2020-02-21 23:27:55 -0500

    create/manage airliner panels

---
+++ Assignment_4/src/Business/CustomerDirectory.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.List;
+
+/**
+ *
+ * @author srush
+ */
+public class CustomerDirectory {
+     private List<Customer> customers;
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @Override
+    public String toString() {
+        return "CustomerDirectory{" + "customers=" + customers + '}';
+    }
+}
