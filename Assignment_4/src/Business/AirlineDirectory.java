b821dd0b8b32779507da1514dd594dbe83126481
bharadwaj-ni <bharadwaj.ni@husky.neu.edu>
2020-02-23 14:48:56 -0500

    create airliners panels

---
+++ Assignment_4/src/Business/AirlineDirectory.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ *
+ * @author srush
+ */
+public class AirlineDirectory {
+
+    private List<Airliner> airlines;
+
+    public List<Airliner> getAirlines() {
+        return airlines;
+    }
+
+    public void setAirlines(List<Airliner> airlines) {
+        this.airlines = airlines;
+    }
+
+}
